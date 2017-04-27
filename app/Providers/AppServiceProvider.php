84b890945d982021121ba0400c5e296b9beec5bc
Luciano Porto <luciano.bapo@gmail.com>
2017-04-27 05:34:32 -0300

    automatic-commit-alias

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Spark;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -35,6 +36,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         if (method_exists($this, 'customizeSpark')) {
             $this->customizeSpark();
         }
